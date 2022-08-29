6e1f422df20a5e98a4cb7d8fbe891c12d83cd890
bobswebsite <105436149+bobswebsite@users.noreply.github.com>
2022-08-29 14:43:31 +0500

    Update Res.js

--- js/Res.js
+++ js/Res.js
@@ -494,6 +494,11 @@ async function diplayData(dataA,stat)
                                   <p class="card-meta font-size-14">${localStorage.getItem("To")}: ${(data[i].citys==localStorage.getItem("To"))?'':(data[i].citys!=null)?data[i].citys:(data[i].To_Airpot==localStorage.getItem("To"))?'':data[i].To_Airpot} ${data[i].To}</p>
                               </div>
                           </div>
+                          <div class="card-rating">
+                            <span class="badge text-white">${(data[i].Star!=null)?data[i].Star:0}/5</span>
+                            <span class="review__text">Average</span>
+                            <span class="rating__text">(${(data[i].Review!=null)?data[i].Review:0} Reviews)</span>
+                        </div>
                           ${(data[i].Airline!=null)?'<div class="flight-time-item take-off">'+
                         '<span class="color-text-2 mr-1">Airline: </span>'+
                           '<img src="'+photoAirlin+'" alt="flight-img"> </div>'
