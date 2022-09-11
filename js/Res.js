5dd1475a761bbc15575fe7a50f0cdead86284979
bobswebsite <105436149+bobswebsite@users.noreply.github.com>
2022-09-11 12:30:06 +0500

    Update Res.js

--- js/Res.js
+++ js/Res.js
@@ -481,7 +481,7 @@ async function diplayData(dataA,stat)
                                            <b><i class="la la-clock-o"></i> ${(data[i].Days!=null)?data[i].Days:(data[i].Nights!=null)?data[i].Nights:''}</b>
                                         </div> 
 										<div class="price-text-flight">
-                                            <b><span class="price-flight"><span  Style=" color: red; font-size:14px"><del>${(data[i].Olde_price!=null)?'£'+data[i].Olde_price+'.00':''}</del></span></b>
+                                            <b><span class="price-flight"><span  Style=" color: red; font-size:14px; position:absolute; right:20px;"><del>${(data[i].Olde_price!=null)?'£'+data[i].Olde_price+'.00':''}</del></span></b>
                                             
                                         </div>
 										
@@ -498,7 +498,7 @@ async function diplayData(dataA,stat)
                                         </div>
                                         
 									<div class="price-text-flight">
-                                            <b><span class="price-flight"><i class="${(data[i].New_price>data[i].Olde_price)?'icono-arrow2-up':(data[i].New_price<data[i].Olde_price)?'icono-arrow2-down':'icono-arrow2-down'}" style="color:${(data[i].New_price>data[i].Olde_price)?'red':(data[i].New_price<data[i].Olde_price)?'green':'grey'}"></i><span Style=" color: #287dfa; font-size:16px">£${(data[i].New_price!=null)?data[i].New_price:(data[i].Total_Price!=null)?data[i].Total_Price:data[i].Total}.00</span></span></b>
+                                            <b><span class="price-flight"><i class="${(data[i].New_price>data[i].Olde_price)?'icono-arrow2-up':(data[i].New_price<data[i].Olde_price)?'icono-arrow2-down':'icono-arrow2-down'}" style="color:${(data[i].New_price>data[i].Olde_price)?'red':(data[i].New_price<data[i].Olde_price)?'green':'grey'}"></i><span Style=" color: #287dfa; font-size:16px; position:absolute; right:20px;">£${(data[i].New_price!=null)?data[i].New_price:(data[i].Total_Price!=null)?data[i].Total_Price:data[i].Total}.00</span></span></b>
                                             
                                         </div>
 											<div style="clear: both;"></div>
